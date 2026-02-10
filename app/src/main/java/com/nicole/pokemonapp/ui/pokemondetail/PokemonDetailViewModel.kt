0d4f0de27c4ad10feb49409e5ed0f026d3e4367e
Nicole <85570874+NicoleGmz@users.noreply.github.com>
2026-02-10 12:51:27 +0000

    Send id from listscreen to detailscreen

--- app/src/main/java/com/nicole/pokemonapp/ui/pokemondetail/PokemonDetailViewModel.kt
+++ app/src/main/java/com/nicole/pokemonapp/ui/pokemondetail/PokemonDetailViewModel.kt
@@ -1,9 +1,15 @@
 package com.nicole.pokemonapp.ui.pokemondetail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
 import com.nicole.domain.detail.usecase.GetPokemonDetailUseCase
+import com.nicole.pokemonapp.navigation.Route
 import com.nicole.pokemonapp.ui.pokemondetail.model.PokemonDetailUiState
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,17 +17,23 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@HiltViewModel
-class PokemonDetailViewModel @Inject constructor(
+@HiltViewModel(assistedFactory = PokemonDetailViewModel.Factory::class)
+class PokemonDetailViewModel @AssistedInject constructor(
+    @Assisted val id: Int,
     private val getPokemonDetail: GetPokemonDetailUseCase
 ) : ViewModel() {
 
+    @AssistedFactory
+    interface Factory{
+        fun create(id: Int): PokemonDetailViewModel
+    }
     private val _uiState = MutableStateFlow<PokemonDetailUiState>(PokemonDetailUiState.DEFAULT)
     val uiState: StateFlow<PokemonDetailUiState> = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            //_uiState.value = getPokemonDetail()
+            println("id: $id")
+           // _uiState.value = getPokemonDetail(id)
         }
     }
 }
--- app/src/main/java/com/nicole/pokemonapp/ui/pokemondetail/view/PokemonDetailScreen.kt
+++ app/src/main/java/com/nicole/pokemonapp/ui/pokemondetail/view/PokemonDetailScreen.kt
@@ -2,6 +2,10 @@ package com.nicole.pokemonapp.ui.pokemondetail.view
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nicole.pokemonapp.ui.pokemondetail.PokemonDetailViewModel
 
 @Composable
@@ -9,4 +13,5 @@ fun PokemonDetailScreen(
     viewModel: PokemonDetailViewModel = hiltViewModel()
 ) {
     // Screen content
+    println( viewModel.id )
 }
