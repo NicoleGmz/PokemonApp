9ed042813efc076d234bf74595e9c01c49d32542
Nicole <85570874+NicoleGmz@users.noreply.github.com>
2026-02-24 10:52:59 +0000

    Search bar added to the list screen

--- app/src/main/java/com/nicole/pokemonapp/ui/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/nicole/pokemonapp/ui/pokemonlist/PokemonListViewModel.kt
@@ -7,8 +7,11 @@ import com.nicole.pokemonapp.ui.pokemonlist.model.PokemonListUiState
 import com.nicole.pokemonapp.ui.pokemonlist.model.toUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,15 +20,34 @@ class PokemonListViewModel @Inject constructor(
     private val getPokemonList: GetPokemonListUseCase
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(PokemonListUiState.DEFAULT)
-    val uiState: StateFlow<PokemonListUiState> = _uiState.asStateFlow()
+    private val _allPokemon = MutableStateFlow(PokemonListUiState.DEFAULT)
+
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
+    //private val _uiState = MutableStateFlow(PokemonListUiState.DEFAULT)
+    val uiState: StateFlow<PokemonListUiState> = combine(
+        _allPokemon,
+        _searchQuery
+    ) { allPokemon, searchQuery ->
+        if (searchQuery.isBlank()) {
+            PokemonListUiState(allPokemon.list)
+        } else {
+            val filteredPokemon = allPokemon.list.filter { pokemon ->
+                pokemon.name.contains(searchQuery, ignoreCase = true)
+            }
+            PokemonListUiState(filteredPokemon)
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), PokemonListUiState.DEFAULT)
 
     init{
         viewModelScope.launch {
-            _uiState.value = getPokemonList().toUiState()
             println("PokemonListViewModel init")
-            println(_uiState.value)
+            _allPokemon.value = getPokemonList().toUiState()
         }
+    }
+
+    fun onSearchQueryChanged(newQuery: String){
+        _searchQuery.value = newQuery
     }
 
 }
