474997e47c27a9f4d896497e653fce892e648c91
Nicole <85570874+NicoleGmz@users.noreply.github.com>
2026-02-24 12:59:23 +0000

    Added filter chips for types of pokemon

--- app/src/main/java/com/nicole/pokemonapp/ui/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/nicole/pokemonapp/ui/pokemonlist/PokemonListViewModel.kt
@@ -23,18 +23,40 @@ class PokemonListViewModel @Inject constructor(
     private val _allPokemon = MutableStateFlow(PokemonListUiState.DEFAULT)
 
     private val _searchQuery = MutableStateFlow("")
+    private val _selectedTypes = MutableStateFlow(emptySet<String>())
+
     val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
+    val selectedTypes: StateFlow<Set<String>> = _selectedTypes.asStateFlow()
+
     //private val _uiState = MutableStateFlow(PokemonListUiState.DEFAULT)
     val uiState: StateFlow<PokemonListUiState> = combine(
         _allPokemon,
-        _searchQuery
-    ) { allPokemon, searchQuery ->
-        if (searchQuery.isBlank()) {
+        _searchQuery,
+        _selectedTypes
+    ) { allPokemon, query, types ->
+        if (query.isBlank() && types.isEmpty()) {
             PokemonListUiState(allPokemon.list)
         } else {
-            val filteredPokemon = allPokemon.list.filter { pokemon ->
+            /*val filteredPokemon = allPokemon.list.filter { pokemon ->
                 pokemon.name.contains(searchQuery, ignoreCase = true)
+                        && (!selectedTypes.isEmpty() || pokemon.types.any { it in selectedTypes })
+            }*/
+
+            var filteredPokemon = allPokemon.list
+
+            if(query.isNotBlank()){
+                filteredPokemon = filteredPokemon.filter { pokemon ->
+                    pokemon.name.contains(query, ignoreCase = true)
+                    pokemon.id.toString().contains(query)
+                }
             }
+
+            if(types.isNotEmpty()){
+                filteredPokemon = filteredPokemon.filter { pokemon ->
+                    pokemon.types.any { it in types }
+                }
+            }
+
             PokemonListUiState(filteredPokemon)
         }
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), PokemonListUiState.DEFAULT)
@@ -48,6 +70,12 @@ class PokemonListViewModel @Inject constructor(
 
     fun onSearchQueryChanged(newQuery: String){
         _searchQuery.value = newQuery
+    }
+
+    fun toggleTypeFilter(type: String) {
+        _selectedTypes.value = _selectedTypes.value.toMutableSet().apply {
+            if (contains(type)) remove(type) else add(type)
+        }
     }
 
 }
